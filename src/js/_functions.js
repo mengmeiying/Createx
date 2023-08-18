0bcf3b067d1f9c4cf9636f86c9cd8f25f058f251
Nadya <meng.meiyingg@gmail.com>
2023-08-18 12:31:27 +0300

    fix js console.logs

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,8 +1,8 @@
 // Данный файл - лишь собрание подключений готовых компонентов.
 // Рекомендуется создавать отдельный файл в папке components и подключать все там
 // Определение операционной системы на мобильных
-import { mobileCheck } from "./functions/mobile-check";
-console.log(mobileCheck())
+// import { mobileCheck } from "./functions/mobile-check";
+// console.log(mobileCheck())
 
 
 // Определение ширины экрана
--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,10 +2,13 @@
 import GraphModal from 'graph-modal';
 const modal = new GraphModal();
 
-document.querySelector('.positions-content__subscribe').addEventListener('click', () => {
-	new GraphModal().open('subscribe');
-});
+if (document.querySelector('.graph-modal')) {
+  document.querySelector('.positions-content__subscribe').addEventListener('click', () => {
+    new GraphModal().open('subscribe');
+  });
 
-document.querySelector('.positions-content__send-cv').addEventListener('click', () => {
-	new GraphModal().open('send-cv');
-});
+  document.querySelector('.positions-content__send-cv').addEventListener('click', () => {
+    new GraphModal().open('send-cv');
+  });
+
+}
